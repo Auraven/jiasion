d87106323667d978d63ea044409af2f897fe7499
Auraven <grandbossanova@gmail.com>
2022-10-13 11:40:59 -0400

    Refactored for use of annotations

--- example/main.py
+++ example/main.py
@@ -2,55 +2,23 @@ from jiasion import Serializable
 import json
 
 class Stats(Serializable):
-    def __init__(self, constitution=0, strength=0, dexterity=0, intelligence=0, wisdom=0, charisma=0, json_object=None):       
-        self.constitution = constitution 
-        self.strength = strength 
-        self.dexterity = dexterity 
-        self.intelligence = intelligence 
-        self.wisdom = wisdom 
-        self.charisma = charisma 
-        if json_object is not None:
-            self.decode(json_object)
+    kda_ratio : float
+    wins : int
 
 class ItemSlot(Serializable):
-    def __init__(self, name='', stack_size=0, json_object=None):       
-        self.name = name 
-        self.stack_size = stack_size
-        if json_object is not None:
-            self.decode(json_object)
+    name : str
+    stack_size : int    
 
 class Player(Serializable):
-    def __init__(self, name='', stats=Stats(), inventory=None, json_object=None):       
-        self.name = name
-        self.stats = stats
-        self.inventory = inventory        
-        if json_object is not None:   
-            self.inventory = [ItemSlot()]    
-            self.decode(json_object)
+    name : str
+    stats : Stats
+    inventory : list[ItemSlot]
 
-inventory = [
-    ItemSlot('Knife', 1),
-    ItemSlot("Fork", 15),
-    ItemSlot("Spoon", 3)
-]
-
-player = Player('Yaga Baba', Stats(12,14,18,8,10,9), inventory)
-print(player.name)
-for item_slot in player.inventory:
-    print(item_slot.name, item_slot.stack_size)
-print(player.stats.dexterity)
-print()
-
-player.name = "Baba Yaga"
-player.inventory[0].name = 'Spork'
-player.stats.dexterity = 20
+with open('player.json', 'r') as jfile:
+    player = Player(json.load(jfile))
+    for item_slot in player.inventory:
+        item_slot.stack_size += 2
+        print(item_slot.name, item_slot.stack_size)
 
 with open('player.json', 'w') as jfile:
     json.dump(player.encode(), jfile, indent=4)
-
-with open('player.json', 'r') as jfile:
-    player = Player(json_object=json.load(jfile))
-    print(player.name)
-    for item_slot in player.inventory:
-        print(item_slot.name, item_slot.stack_size) 
-    print(player.stats.dexterity)
--- src/jiasion.py
+++ src/jiasion.py
@@ -1,32 +1,40 @@
-import copy
+class Serializable():   
+    def __init__(self, json_object : dict = None, **kwargs):
+        if json_object is not None: #Decode JSON into Class
+            self.decode(json_object)
+        for key, value in kwargs.items(): #Override Class Attributes with values
+            self.__setattr__(key, value)
+        for attr_name in self.__annotations__: #Ensure that Attributes exist, even if not set
+            if attr_name not in self.__dict__:
+                self.__setattr__(attr_name, None)
 
-class Serializable():
     def decode(self, json_object : dict):
-        for attr_name in json_object:
-            attr = self.__getattribute__(attr_name)
-            if issubclass(type(attr), Serializable):
+        for attr_name , attr_type in self.__annotations__.items():
+            attr = attr_type() #Instantiate default subclass Serializable
+            self.__setattr__(attr_name, attr) #Create default attribute of name and type, even if not in the JSON            
+            
+            if issubclass(attr_type, Serializable): #Is Serializable
                 attr.decode(json_object[attr_name])
-            elif issubclass(type(attr), list) and len(attr) == 1 and issubclass(type(attr[0]), Serializable):
-                attr_list = []
-                for sub_object in json_object[attr_name]:
-                    attr_copy = copy.deepcopy(attr[0])
-                    attr_copy.decode(sub_object)
-                    attr_list.append(attr_copy)
-                self.__setattr__(attr_name, attr_list)
-            else:
-                self.__setattr__(attr_name, json_object[attr_name])
+            elif isinstance(attr, list) and issubclass(attr_type.__args__[0], Serializable): #Check if list of type Serializable
+                serial_type = attr_type.__args__[0]
+                serial_list = []
+                for object_element in json_object[attr_name]:
+                    serial_list.append(serial_type(object_element))
+                self.__setattr__(attr_name, serial_list)
+            else: #Basic Key Value
+                self.__setattr__(attr_name, json_object[attr_name])           
    
     def encode(self) -> dict:
         json_object = {}
-        for attr_name in self.__dict__:
+        for attr_name , attr_type in self.__annotations__.items():
             attr = self.__getattribute__(attr_name)
-            if issubclass(type(attr), Serializable):
+            if issubclass(attr_type, Serializable):
                 json_object[attr_name] = attr.encode()
-            elif issubclass(type(attr), list) and len(attr) > 0 and issubclass(type(attr[0]), Serializable):
-                attr_list = []
-                for sub_object in attr:
-                    attr_list.append(sub_object.encode())
-                json_object[attr_name] = attr_list
+            elif isinstance(attr, list) and issubclass(attr_type.__args__[0], Serializable):
+                serial_list = []
+                for object_element in attr:
+                    serial_list.append(object_element.encode())
+                json_object[attr_name] = serial_list
             else:
                 json_object[attr_name] = attr
         return json_object
